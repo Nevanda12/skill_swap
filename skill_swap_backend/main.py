0eb4d257faac4b239f1c4164dc4fc47098c46444
Nevanda D.Evrilian <165225251+Nevanda12@users.noreply.github.com>
2026-07-26 17:00:24 +0700

    tambah kolom

--- skill_swap_backend/main.py
+++ skill_swap_backend/main.py
@@ -92,6 +92,9 @@ def create_tables():
         full_name VARCHAR(100) NOT NULL,
         email VARCHAR(100) UNIQUE NOT NULL,
         password_hash VARCHAR(255) NOT NULL,
+        is_verified BOOLEAN DEFAULT FALSE,
+        otp_code VARCHAR(10),
+        otp_expires_at DATETIME,
         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     );
     """)
